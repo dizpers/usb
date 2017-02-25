3a7a8b963ac05f445a27256beffc0377baf4de79
dizpers <dizpers@gmail.com>
2017-02-25 22:01:09 +0700

    Test shorten functionality

--- usb/tests/test_shortener.py
+++ usb/tests/test_shortener.py
@@ -1,10 +1,12 @@
 from unittest import TestCase
 
+from usb.shortener import shorten
+
 
 class ShortenerTestCase(TestCase):
 
-    def test_long_to_short(self):
-        pass
-
-    def test_short_to_long(self):
-        pass
+    def test_shorten(self):
+        long_url = 'https://www.youtube.com/watch?v=Y21VecIIdBI'
+        short_url = shorten(long_url)
+        self.assertNotEqual(long_url, short_url)
+        self.assertRegex(short_url, r'^[a-zA-Z0-9]{6,}$')
