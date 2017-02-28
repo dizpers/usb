65776c23adf28ef489907d686d6ceee49049a0ab
dizpers <dizpers@gmail.com>
2017-02-28 20:33:42 +0700

    Add test methods for shortening API endpoints

--- usb/tests/test_api.py
+++ usb/tests/test_api.py
@@ -25,7 +25,34 @@ class APITestCase(TestCase):
         pass
 
     def test_create_short_link(self):
-        pass
+        long_url = 'https://www.youtube.com/watch?v=Y21VecIIdBI'
+        response = self.client.post('/links', {'url': long_url})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.headers['Content-Type'], 'application/json')
+        data = json.loads(response.data)
+        self.assertIn('url', data)
+        short_url = data['url']
+        self.assertNotEqual(long_url, short_url)
+        short_id = short_url.split('/')[-1]
+        self.assertRegex(short_id, r'^[a-zA-Z0-9]{8,}$')
+
+    def test_create_short_link_for_already_shortened(self):
+        long_url = 'https://www.youtube.com/watch?v=Y21VecIIdBI'
+        response = self.client.post('/links', {'url': long_url})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.headers['Content-Type'], 'application/json')
+        data_from_first_call = json.loads(response.data)
+        self.assertIn('url', data_from_first_call)
+        short_url = data_from_first_call['url']
+        self.assertNotEqual(long_url, short_url)
+        short_id = short_url.split('/')[-1]
+        self.assertRegex(short_id, r'^[a-zA-Z0-9]{8,}$')
+        response = self.client.post('/links', {'url': long_url})
+        self.assertEqual(response.status_code, 409)
+        self.assertEqual(response.headers['Content-Type'], 'application/json')
+        data_from_second_call = json.loads(response.data)
+        self.assertEqual(data_from_first_call, data_from_second_call)
+
 
     def test_update_short_link(self):
         pass
