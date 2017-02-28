97e960cba02b6d163f15ad41177aed71acdfbd35
dizpers <dizpers@gmail.com>
2017-02-28 15:15:48 +0700

    Draft application creation in API test case

--- usb/tests/test_api.py
+++ usb/tests/test_api.py
@@ -1,7 +1,19 @@
 from unittest import TestCase
 
+from usb import create_application
+from usb import config
+
+
 
 class APITestCase(TestCase):
+
+    def setUp(self):
+        self.app = create_application(config.test)
+        self.client = self.app.test_client()
+        self.app.init_db()
+
+    def tearDown(self):
+        self.app.drop_db()
 
     def test_redirect_from_index_namespace(self):
         pass
