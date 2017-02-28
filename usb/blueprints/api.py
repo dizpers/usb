6ca80883b4283f731463703c040327efe17fb994
dizpers <dizpers@gmail.com>
2017-02-28 20:44:00 +0700

    Draft shortening long url API method

--- usb/blueprints/api.py
+++ usb/blueprints/api.py
@@ -6,3 +6,8 @@ api = Blueprint('api', __name__)
 @api.route('/links')
 def get_links():
     return jsonify({}), 200
+
+
+@api.route('/links', methods=['POST'])
+def shorten_url():
+    return jsonify({}), 200
