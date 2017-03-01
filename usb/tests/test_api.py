17f4d0882660e56a2f48a6bf7d2ac3d1fcafbd35
dizpers <dizpers@gmail.com>
2017-03-01 22:33:03 +0700

    Draft tests of actual redirecting

--- usb/tests/test_api.py
+++ usb/tests/test_api.py
@@ -43,11 +43,25 @@ class APITestCase(TestCase):
         self.assertFalse(url.query)
         self.assertFalse(url.fragment)
 
-    def test_redirect_from_index_namespace(self):
+    def _test_redirect_desktop(self, url):
         pass
 
-    def test_redirect_from_links_namespace(self):
+    def _test_redirect_tablet(self, url):
         pass
+
+    def _test_redirect_mobile(self, url):
+        pass
+
+    def _test_redirect(self, url):
+        self._test_redirect_desktop(url)
+        self._test_redirect_tablet(url)
+        self._test_redirect_mobile(url)
+
+    def test_redirect_from_index_namespace(self):
+        self._test_redirect('/')
+
+    def test_redirect_from_links_namespace(self):
+        self._test_redirect('/links/')
 
     def test_create_short_link(self):
         long_url = 'https://www.youtube.com/watch?v=Y21VecIIdBI'
