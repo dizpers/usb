c02524c4785cc75b276744be2d207e59d7a52bcb
dizpers <dizpers@gmail.com>
2017-03-01 23:18:04 +0700

    Extend test of updating short URL API method - check for count and datetime

--- usb/tests/api/test_update_short_url.py
+++ usb/tests/api/test_update_short_url.py
@@ -10,11 +10,11 @@ class UpdateShortUrlTestCase(APITestCase):
     def setUp(self):
         super(UpdateShortUrlTestCase, self).setUp()
 
-        dt = datetime.now()
+        self.dt = datetime.now()
         redirects = (
-            Redirect('aaaaaaaa', DeviceType.DESKTOP, 'http://domain1.com/path?q=a', count=10, datetime=dt),
-            Redirect('aaaaaaaa', DeviceType.TABLET, 'http://tablet.domain1.com/path?q=a', count=20, datetime=dt),
-            Redirect('aaaaaaaa', DeviceType.MOBILE, 'http://mobile.domain1.com/path?q=a', count=30, datetime=dt)
+            Redirect('aaaaaaaa', DeviceType.DESKTOP, 'http://domain1.com/path?q=a', count=10, datetime=self.dt),
+            Redirect('aaaaaaaa', DeviceType.TABLET, 'http://tablet.domain1.com/path?q=a', count=20, datetime=self.dt),
+            Redirect('aaaaaaaa', DeviceType.MOBILE, 'http://mobile.domain1.com/path?q=a', count=30, datetime=self.dt)
         )
 
         for redirect in redirects:
@@ -31,12 +31,13 @@ class UpdateShortUrlTestCase(APITestCase):
         self.assertEqual(data, {})
 
         tablet_redirects = Redirect.query.filter_by(short='aaaaaaaa', type=DeviceType.TABLET).all()
-        self.assertTrue(tablet_redirects)
         self.assertEqual(len(tablet_redirects), 1)
         tablet_redirect = tablet_redirects[0]
         self.assertEqual(tablet_redirect.short, 'aaaaaaaa')
         self.assertEqual(tablet_redirect.type, DeviceType.TABLET)
         self.assertEqual(tablet_redirect.url, 'http://gov.us/elect/president?name=')
+        self.assertEqual(tablet_redirect.count, 0)
+        self.assertNotEqual(tablet_redirect.datetime. self.dt)
 
     def test_update_short_url_no_such_short_id(self):
         response = self._patch_json('/urls/bbb', {'tablet': 'http://gov.us/elect/president?name='})
