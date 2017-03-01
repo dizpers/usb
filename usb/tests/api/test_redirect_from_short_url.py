59a166faba231ee010740cfd983407e1260726ce
dizpers <dizpers@gmail.com>
2017-03-02 00:21:06 +0700

    Add test to check count of redirects

--- usb/tests/api/test_redirect_from_short_url.py
+++ usb/tests/api/test_redirect_from_short_url.py
@@ -53,3 +53,23 @@ class RedirectFromShortURLTestCase(APITestCase):
 
     def test_redirect_from_links_namespace(self):
         self._test_redirect('/urls/')
+
+    def test_redirect_increase_count(self):
+        # 3 desktop
+        self._test_redirect_desktop('/')
+        self._test_redirect_desktop('/')
+        self._test_redirect_desktop('/')
+
+        # 0 tablet
+
+        # 1 mobile
+        self._test_redirect_mobile('/')
+
+        redirects = Redirect.query.filter_by(short='aaaaaaaa').all()
+        for redirect in redirects:
+            if redirect.type == DeviceType.DESKTOP:
+                self.assertEqual(redirect.count, 3)
+            if redirect.type == DeviceType.TABLET:
+                self.assertEqual(redirect.count, 0)
+            if redirect.type == DeviceType.MOBILE:
+                self.assertEqual(redirect.count, 1)
