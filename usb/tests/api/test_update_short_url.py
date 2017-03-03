5003c03f85df0cdfc95587c1a69859308843db5e
dizpers <dizpers@gmail.com>
2017-03-03 14:42:39 +0700

    Add to do with idea of further refactoring

--- usb/tests/api/test_update_short_url.py
+++ usb/tests/api/test_update_short_url.py
@@ -34,6 +34,7 @@ class UpdateShortUrlTestCase(APITestCase):
         self.assertEqual(len(tablet_redirects), 1)
         tablet_redirect = tablet_redirects[0]
         self.assertEqual(tablet_redirect.short, 'aaaaaaaa')
+        # TODO: get device code from actual class?
         self.assertEqual(tablet_redirect.type, DeviceType.TABLET)
         self.assertEqual(tablet_redirect.url, 'http://gov.us/elect/president?name=')
         self.assertEqual(tablet_redirect.count, 0)
