011a2c8a657d24d7bf0cfc9b982bd6b1f23e29d3
dizpers <dizpers@gmail.com>
2017-03-03 14:36:41 +0700

    Refactor update short url test case according to new polymorphic models approach

--- usb/tests/api/test_update_short_url.py
+++ usb/tests/api/test_update_short_url.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import json
 
-from usb.models import db, Redirect, DeviceType
+from usb.models import db, DeviceType, Desktop, Tablet, Mobile
 from usb.tests.base import APITestCase
 
 
@@ -12,9 +12,9 @@ class UpdateShortUrlTestCase(APITestCase):
 
         self.dt = datetime.now()
         redirects = (
-            Redirect('aaaaaaaa', DeviceType.DESKTOP, 'http://domain1.com/path?q=a', count=10, datetime=self.dt),
-            Redirect('aaaaaaaa', DeviceType.TABLET, 'http://tablet.domain1.com/path?q=a', count=20, datetime=self.dt),
-            Redirect('aaaaaaaa', DeviceType.MOBILE, 'http://mobile.domain1.com/path?q=a', count=30, datetime=self.dt)
+            Desktop('aaaaaaaa', 'http://domain1.com/path?q=a', self.dt, 10),
+            Tablet('aaaaaaaa', 'http://tablet.domain1.com/path?q=a', self.dt, 20),
+            Mobile('aaaaaaaa', 'http://mobile.domain1.com/path?q=a', self.dt, 30)
         )
 
         for redirect in redirects:
@@ -30,7 +30,7 @@ class UpdateShortUrlTestCase(APITestCase):
         data = json.loads(response.data)
         self.assertEqual(data, {})
 
-        tablet_redirects = Redirect.query.filter_by(short='aaaaaaaa', type=DeviceType.TABLET).all()
+        tablet_redirects = Tablet.query.filter_by(short='aaaaaaaa').all()
         self.assertEqual(len(tablet_redirects), 1)
         tablet_redirect = tablet_redirects[0]
         self.assertEqual(tablet_redirect.short, 'aaaaaaaa')
